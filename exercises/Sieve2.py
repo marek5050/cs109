ce5fe4991a3f1184930d91b268bbc80035e0b99a
Glenn Downing <downing@cs.utexas.edu>
2015-04-05 15:44:55 -0500

    another commit

--- exercises/Sieve2.py
+++ exercises/Sieve2.py
@@ -7,6 +7,21 @@
 from math     import sqrt
 from unittest import main, TestCase
 
+def sieve (n) :
+    a    = [1] * (n + 1)
+    a[1] = 0
+    a[2] = 1
+    for i in range(4, n + 1, 2) :
+        a[i] = 0
+    for i in range(3, int(sqrt(n)) + 1, 2) :
+        j = i
+        while j <= n :
+            a[j]  = 0
+            j    *= i
+    return a[1:]
+
+print(sieve(11))
+
 class MyUnitTestss (TestCase) :
     def test_0 (self) :
         self.assertEqual(sieve(0), [])
@@ -44,5 +59,5 @@ class MyUnitTestss (TestCase) :
     def test_11 (self) :
         self.assertEqual(sieve(11), [0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1])
 
-if __name__ == "__main__" :
-    main()
+#if __name__ == "__main__" :
+#    main()
