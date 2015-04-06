3dc2f0b6e04880ed915f8d79f69d89fe5d84acf6
Glenn Downing <downing@cs.utexas.edu>
2015-04-06 16:55:05 -0500

    another commit

--- exercises/Sieve.py
+++ exercises/Sieve.py
@@ -10,35 +10,38 @@ from math     import sqrt
 from unittest import main, TestCase
 
 class MyUnitTestss (TestCase) :
+    def setUp (self) :
+        self.a = [False, True, True, False, True, False, True, False, False, False, True]
+
     def test_2 (self) :
-        self.assertEqual(sieve(2), [False, True])
+        self.assertEqual(sieve(2)[1:], self.a[:2])
 
     def test_3 (self) :
-        self.assertEqual(sieve(3), [False, True, True])
+        self.assertEqual(sieve(3)[1:], self.a[:3])
 
     def test_4 (self) :
-        self.assertEqual(sieve(4), [False, True, True, False])
+        self.assertEqual(sieve(4)[1:], self.a[:4])
 
     def test_5 (self) :
-        self.assertEqual(sieve(5), [False, True, True, False, True])
+        self.assertEqual(sieve(5)[1:], self.a[:5])
 
     def test_6 (self) :
-        self.assertEqual(sieve(6), [False, True, True, False, True, False])
+        self.assertEqual(sieve(6)[1:], self.a[:6])
 
     def test_7 (self) :
-        self.assertEqual(sieve(7), [False, True, True, False, True, False, True])
+        self.assertEqual(sieve(7)[1:], self.a[:7])
 
     def test_8 (self) :
-        self.assertEqual(sieve(8), [False, True, True, False, True, False, True, False])
+        self.assertEqual(sieve(8)[1:], self.a[:8])
 
     def test_9 (self) :
-        self.assertEqual(sieve(9), [False, True, True, False, True, False, True, False, False])
+        self.assertEqual(sieve(9)[1:], self.a[:9])
 
     def test_10 (self) :
-        self.assertEqual(sieve(10), [False, True, True, False, True, False, True, False, False, False])
+        self.assertEqual(sieve(10)[1:], self.a[:10])
 
     def test_11 (self) :
-        self.assertEqual(sieve(11), [False, True, True, False, True, False, True, False, False, False, True])
+        self.assertEqual(sieve(11)[1:], self.a[:11])
 
 if __name__ == "__main__" :
     main()
--- exercises/Sieve3.py
+++ exercises/Sieve3.py
@@ -14,34 +14,34 @@ class MyUnitTestss (TestCase) :
         self.a = [1, 2, 3, 2, 5, 2, 7, 2, 3, 2, 11]
 
     def test_2 (self) :
-        self.assertEqual(sieve(2), self.a[:2])
+        self.assertEqual(sieve(2)[1:], self.a[:2])
 
     def test_3 (self) :
-        self.assertEqual(sieve(3), self.a[:3])
+        self.assertEqual(sieve(3)[1:], self.a[:3])
 
     def test_4 (self) :
-        self.assertEqual(sieve(4), self.a[:4])
+        self.assertEqual(sieve(4)[1:], self.a[:4])
 
     def test_5 (self) :
-        self.assertEqual(sieve(5), self.a[:5])
+        self.assertEqual(sieve(5)[1:], self.a[:5])
 
     def test_6 (self) :
-        self.assertEqual(sieve(6), self.a[:6])
+        self.assertEqual(sieve(6)[1:], self.a[:6])
 
     def test_7 (self) :
-        self.assertEqual(sieve(7), self.a[:7])
+        self.assertEqual(sieve(7)[1:], self.a[:7])
 
     def test_8 (self) :
-        self.assertEqual(sieve(8), self.a[:8])
+        self.assertEqual(sieve(8)[1:], self.a[:8])
 
     def test_9 (self) :
-        self.assertEqual(sieve(9), self.a[:9])
+        self.assertEqual(sieve(9)[1:], self.a[:9])
 
     def test_10 (self) :
-        self.assertEqual(sieve(10), self.a[:10])
+        self.assertEqual(sieve(10)[1:], self.a[:10])
 
     def test_11 (self) :
-        self.assertEqual(sieve(11), self.a[:11])
+        self.assertEqual(sieve(11)[1:], self.a[:11])
 
 if __name__ == "__main__" :
     main()
