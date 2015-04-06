e7cc06f03c5b1b63cca61b315613c9b4c8a52f8f
Glenn Downing <downing@cs.utexas.edu>
2015-04-06 07:39:53 -0500

    another commit

--- exercises/Sieve2.py
+++ exercises/Sieve2.py
@@ -17,41 +17,44 @@ def sieve (n) :
     for i in range(4, n + 1, 2) :
         a[i] = False
     for i in range(3, int(sqrt(n)) + 1, 2) :
-        if a[i] == True :
+        if a[i] :
             for j in range(i ** 2, n + 1, i) :
                 a[j] = False
     return a[1:]
 
 class MyUnitTestss (TestCase) :
+    def setUp (self) :
+        self.a = [False, True, True, False, True, False, True, False, False, False, True]
+
     def test_2 (self) :
-        self.assertEqual(sieve(2), [False, True])
+        self.assertEqual(sieve(2), self.a[:2])
 
     def test_3 (self) :
-        self.assertEqual(sieve(3), [False, True, True])
+        self.assertEqual(sieve(3), self.a[:3])
 
     def test_4 (self) :
-        self.assertEqual(sieve(4), [False, True, True, False])
+        self.assertEqual(sieve(4), self.a[:4])
 
     def test_5 (self) :
-        self.assertEqual(sieve(5), [False, True, True, False, True])
+        self.assertEqual(sieve(5), self.a[:5])
 
     def test_6 (self) :
-        self.assertEqual(sieve(6), [False, True, True, False, True, False])
+        self.assertEqual(sieve(6), self.a[:6])
 
     def test_7 (self) :
-        self.assertEqual(sieve(7), [False, True, True, False, True, False, True])
+        self.assertEqual(sieve(7), self.a[:7])
 
     def test_8 (self) :
-        self.assertEqual(sieve(8), [False, True, True, False, True, False, True, False])
+        self.assertEqual(sieve(8), self.a[:8])
 
     def test_9 (self) :
-        self.assertEqual(sieve(9), [False, True, True, False, True, False, True, False, False])
+        self.assertEqual(sieve(9), self.a[:9])
 
     def test_10 (self) :
-        self.assertEqual(sieve(10), [False, True, True, False, True, False, True, False, False, False])
+        self.assertEqual(sieve(10), self.a[:10])
 
     def test_11 (self) :
-        self.assertEqual(sieve(11), [False, True, True, False, True, False, True, False, False, False, True])
+        self.assertEqual(sieve(11), self.a[:11])
 
 if __name__ == "__main__" :
     main()
